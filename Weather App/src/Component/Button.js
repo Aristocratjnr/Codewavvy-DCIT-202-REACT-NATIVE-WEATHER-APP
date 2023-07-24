dc7f9efa6cd3e1c0f00ef5b0bd96719ff791bdb4
nazifawal1 <104230393+nazifawal1@users.noreply.github.com>
2023-07-24 10:42:53 -0400

    Update Button.js

--- Weather App/src/Component/Button.js
+++ Weather App/src/Component/Button.js
@@ -1,3 +1,4 @@
+//imported dependencies
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { DEVICE_WIDTH, wp } from '../Utils/ResponsiveLayout';
